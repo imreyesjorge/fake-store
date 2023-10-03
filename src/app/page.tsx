cf4175156efa64d76e019930b4732a97043119ab
Jorge Reyes <40706868+imreyesjorge@users.noreply.github.com>
2023-10-02 18:27:57 -0600

    Connect the login with the API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,84 @@
 "use client";
 
 import { Button, Input } from "@nextui-org/react";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
+import { FormData } from "./types";
+import toast from "react-hot-toast";
 
 export default function LoginScreen() {
+  const [formData, setFormData] = useState<FormData>({
+    username: "",
+    password: "",
+  });
+
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+
+    if (!(formData.username && formData.password)) {
+      toast.error(`Please provide a valid username and password.`);
+      return;
+    }
+
+    try {
+      const response = await fetch("https://fakestoreapi.com/auth/login", {
+        method: "POST",
+        body: JSON.stringify({
+          username: "mor_2314",
+          password: "83r5^_",
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to connect with the server`);
+      }
+
+      const responseData = await response.json();
+
+      // Set the token to the localStorage
+      localStorage.setItem("token", responseData.token);
+    } catch (error) {
+      toast.error(error.message);
+
+      // Use `anon` as a token if the service failed
+      localStorage.setItem("token", "anon");
+    }
+  };
+
+  const handleValueChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setFormData((currentState) => ({
+      ...currentState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
-    <form className="w-[400px] mx-auto flex flex-col gap-[20px]">
+    <form
+      className="w-[400px] mx-auto flex flex-col gap-[20px]"
+      onSubmit={handleFormSubmit}
+    >
       <div className="text-center flex flex-col gap-[10px]">
         <h1 className="text-2xl font-bold">We’re glad to see you again!</h1>
         <p className="text-sm text-slate-600">
           Use your credentials to access the store
         </p>
       </div>
-      <Input type="text" label="username" variant="bordered" />
-      <Input type="password" label="password" variant="bordered" />
-      <Button fullWidth size="lg" color="primary" disableRipple>
+      <Input
+        type="text"
+        label="username"
+        name="username"
+        variant="bordered"
+        value={formData.username}
+        onChange={handleValueChange}
+      />
+      <Input
+        type="password"
+        label="password"
+        name="password"
+        variant="bordered"
+        value={formData.password}
+        onChange={handleValueChange}
+      />
+      <Button fullWidth size="lg" color="primary" disableRipple type="submit">
         Login
       </Button>
     </form>
--- src/context/TPProviders/TPProviders.tsx
+++ src/context/TPProviders/TPProviders.tsx
@@ -2,7 +2,25 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { TPProvidersProps } from "./types";
+import { Toaster } from "react-hot-toast";
 
 export function TPProviders({ children }: TPProvidersProps) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <>
+        {children}
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              borderRadius: "10px",
+              background: "#333",
+              color: "#fff",
+              padding: "10px 20px",
+            },
+          }}
+        />
+      </>
+    </NextUIProvider>
+  );
 }
